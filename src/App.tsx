17c0d8b585df801d34f3f7dd372cdcec5b31d52d
Weliton Freitas <notilew@gmail.com>
2025-01-30 12:05:50 -0300

    initical commit of frontend

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+
+function App() {
+  
+  return (
+      <div style={{width: '100%', height: '98vh'}}>
+        <iframe width="100%" height="100%" frameBorder="0" scrolling="No"  src="https://pixnopdv.com.br/cliente/login.html"></iframe>
+      </div>
+  )
+}
+
+export default App
